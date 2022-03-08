ea352fac083ee6861bf44451d61dbe0100008b9d
Ivan <drugov.iv@edu.spbstu.ru>
2022-03-08 14:13:44 +0300

    Указал наиболее абстрактный тип и убрал ненужные поля.

--- task/src/main/java/task1/Group.java
+++ task/src/main/java/task1/Group.java
@@ -1,12 +1,13 @@
 package task1;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Group {
     private final int numb;
-    private final ArrayList<String> student;
-    private final ArrayList<String> subjects;
-    private final ArrayList<Integer> marks;
+    private final List<String> student;
+    private final List<String> subjects;
+    private final List<Integer> marks;
 
     public Group(int numb, ArrayList<String> student, ArrayList<String> subjects, ArrayList<Integer> marks) {
         this.numb = numb;
@@ -15,7 +16,7 @@ public class Group {
         this.marks = marks;
     }
     public int a;
-    public ArrayList<String> delName (String delname) {
+    public List<String> delName (String delname) {
         if (student.contains(delname))
             a = student.indexOf(delname) + 1;
         student.remove(delname);
@@ -25,17 +26,17 @@ public class Group {
         }
         return (student);
     }
-    public ArrayList<String> addName (String addname) {
+    public List<String> addName (String addname) {
         if (!student.contains(addname))
             student.add(addname);
         return student;
     }
-    public ArrayList<String> addSubject (String addsubject) {
+    public List<String> addSubject (String addsubject) {
         if (!subjects.contains(addsubject))
             subjects.add(addsubject);
         return subjects;
     }
-    public ArrayList<String> dellSubject (String dellsub) {
+    public List<String> dellSubject (String dellsub) {
         if (subjects.contains(dellsub))
             a = subjects.indexOf(dellsub) + 1;
         subjects.remove(dellsub);
@@ -45,25 +46,18 @@ public class Group {
         }
         return subjects;
     }
-    public int idp;
-    public int ids;
-    public ArrayList<Integer> test = new ArrayList<>();
     public ArrayList<Integer> dellmark (String stud, String sub){
-        test = new ArrayList<>();
-        idp = subjects.indexOf(sub);
-        ids = student.indexOf(stud);
-        marks.set(idp+ids*subjects.size(), null);
-        for (int i=ids* subjects.size(); i<(ids+1)* subjects.size();i++){
+        ArrayList<Integer> test = new ArrayList<>();
+        marks.set(subjects.indexOf(sub)+student.indexOf(stud)*subjects.size(), null);
+        for (int i=student.indexOf(stud)* subjects.size(); i<(student.indexOf(stud)+1)* subjects.size();i++){
             test.add(marks.get(i));
         }
         return test;
     }
     public ArrayList<Integer> changemark (Integer dm, String stud, String sub){
-        test = new ArrayList<>();
-        idp = subjects.indexOf(sub);
-        ids = student.indexOf(stud);
-        marks.set(idp+ids*subjects.size(), dm);
-        for (int i=ids* subjects.size(); i<(ids+1)* subjects.size();i++){
+        ArrayList<Integer> test = new ArrayList<>();
+        marks.set(subjects.indexOf(sub)+student.indexOf(stud)*subjects.size(), dm);
+        for (int i=student.indexOf(stud)* subjects.size(); i<(student.indexOf(stud)+1)* subjects.size();i++){
             test.add(marks.get(i));
         }
         return test;
